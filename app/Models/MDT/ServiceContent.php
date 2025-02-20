c98574b5e5b4ac174b5c3ab00b1415fd97158970
DmitrijM1983 <dmitrij.m.183@yandex.ru>
2025-02-20 15:40:01 +0300

    image field make required

--- app/Models/MDT/ServiceContent.php
+++ app/Models/MDT/ServiceContent.php
@@ -4,6 +4,8 @@ namespace App\Models\MDT;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ServiceContent extends Model
 {
@@ -17,4 +19,14 @@ class ServiceContent extends Model
         'preview',
         'video'
     ];
+
+    public function service(): BelongsTo
+    {
+        return $this->belongsTo(Service::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'service_id', 'id');
+    }
 }
