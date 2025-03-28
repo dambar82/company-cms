61337e372201f4a5405b49582ccb20a4d4375fc5
DmitrijM1983 <dmitrij.m.183@yandex.ru>
2025-03-28 17:31:36 +0300

    amina feedback resource changed

--- app/Http/Resources/AminaFeedbackResource.php
+++ app/Http/Resources/AminaFeedbackResource.php
@@ -15,6 +15,7 @@ class AminaFeedbackResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'creator' => $this->creator,
             'organization' => $this->organization,
             'job_title' => $this->job_title,
